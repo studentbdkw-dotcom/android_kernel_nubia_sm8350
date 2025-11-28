97b63f93fa0cec31d34e91254281c7727b06a6d8
studentbdkw-dotcom <studentbdkw@gmail.com>
2025-11-28 23:12:50 +0300

    Fix formatting issue in sde_hw_dsc_1_2.c

--- techpack/display/msm/sde/sde_hw_dsc_1_2.c
+++ techpack/display/msm/sde/sde_hw_dsc_1_2.c
@@ -78,7 +78,7 @@ static int _dsc_calc_ob_max_addr(struct sde_hw_dsc *hw_dsc, int num_ss)
 }
 
 static inline _dsc_subblk_offset(struct sde_hw_dsc *hw_dsc, int s_id,
-		u32 *idx)
+                                 u32 *idx)
 {
 	const struct sde_dsc_sub_blks *sblk;
 
